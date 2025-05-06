fd4f98fe9f5404e1db7047829f7c4baa48cb7d6f
Ash <ashwinravikumarv@gmail.com>
2025-05-06 23:34:11 +0100

    Re-enabled TG login

--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,7 @@ root.render(
       <PrivyProvider
         appId={process.env.REACT_APP_PRIVY_APP_ID!}
         config={{
-          loginMethods: ['email', 'twitter'],
+          loginMethods: ['email', 'twitter', 'telegram'],
           appearance: {
             theme: 'light',
             accentColor: '#007AFF',
@@ -37,7 +37,7 @@ root.render(
     <PrivyProvider
       appId={process.env.REACT_APP_PRIVY_APP_ID!}
       config={{
-        loginMethods: ['email', 'twitter'],
+        loginMethods: ['email', 'twitter', 'telegram'],
         appearance: {
           theme: 'light',
           accentColor: '#007AFF',
